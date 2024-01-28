bacd0a815943cb89f18ba75cff049c6db9aa4615
Frikky <frikky@shuffler.io>
2024-01-28 03:54:33 +0100

    Added a concurrency file to test out multiprocessing of IOCs. No obvious improvement with it due to GIL. Removing IPv6 by default = 26% improvement~. Should optimize usage based on user ask

--- shuffle-tools/1.2.0/src/app.py
+++ shuffle-tools/1.2.0/src/app.py
@@ -2446,170 +2446,12 @@ class Tools(AppBase):
             return {"success":"false","message":str(e)}
 
         return {"success":"true","output": stdout.read().decode(errors='ignore')}
-    
-    def split_list(self, arr, length):
-
-        num_arrays = len(arr) // length
-        remainder = len(arr) % length
-
-        arrays = []
-        start = 0
-
-        for _ in range(num_arrays):
-            sub_array = arr[start:start+length]
-            arrays.append(sub_array)
-            start += length
-
-        if remainder > 0:
-            sub_array = arr[start:]
-            arrays.append(sub_array)
-
-        return arrays
-    
-    def split_text(self,text):
-        lines = text.split("\n")
-        num = 10
-        new_text = ""
-        new_text_list = []
-        for line in lines:
-            num -= 1
-            new_text += line + "\n"
-            if num == 0:
-                new_text_list.append(new_text.strip("\n"))
-                num = 10
-                new_text = ""
-        return new_text_list
-    
-    def _with_concurency(self,txt_data, ioc_types):
-
-        cpu_count = multiprocessing.cpu_count()
-        #print("cpu count:",cpu_count)
-        start = time.perf_counter()
-        executor = concurrent.futures.ProcessPoolExecutor(cpu_count)
-        futures = [executor.submit(find_iocs, row, included_ioc_types=ioc_types) for row in txt_data]
-        results = [future.result() for future in futures]
-        #print("Total time taken:",time.perf_counter()-start)
-        return self._format_result(results)
-    
-    def _format_result(self,result):
-        final_result = {}
-        
-        for res in result:
-            for key,val in res.items():
-                if key in final_result:
-                    if isinstance(val, list) and len(val) > 0:
-                        for i in val:
-                            final_result[key].append(i)
-                    elif isinstance(val, dict):
-                        #print(key,":::",val)
-                        if key in final_result:
-                            if isinstance(val, dict):
-                                for k,v in val.items():
-                                    #print("k:",k,"v:",v)
-                                    val[k].append(v)
-                        #print(val)
-                    #final_result[key].append([i for i in val if len(val) > 0])
-                else:
-                    final_result[key] = val
-
-        return final_result
-    
-    def quick_parse_ioc(self, file_id, input_type=None):
-        file_data = self.get_file(file_id)
-
-        DEFAULT_IOC_TYPES = [
-            "cves",
-            "domains",
-            "ipv4s",
-            "md5s",
-            "sha1s",
-            "sha256s",
-            "urls"
-            "email_addresses",
-        ]
-
-        if input_type == "":
-            input_type = DEFAULT_IOC_TYPES
-        else:
-            input_type = input_type.split(",")
-                
-        if file_data.get("data"):
-            file_data = file_data["data"].decode()
-        else:
-            return {"success":"false","message":"file not found"}
-
-        file_data = self.split_text(file_data)
-        res = self._with_concurency(file_data, ioc_types=input_type)
-        return res
-
-    def parse_ioc_new(self, input_string, input_type="all"):
-        ioc_types = ["domains", "urls", "email_addresses", "ipv6s", "ipv4s", "ipv4_cidrs", "md5s", "sha256s", "sha1s", "cves"]
-        input_string = str(input_string)
-        #if input_type == "":
-        #    input_type = "all"
-        #else:
-        #    input_type = input_type.split(",")
-        #    for item in input_type:
-        #        item = item.strip()
-
-        #    ioc_types = input_type
-
-        file_data = self.split_text(input_string)
-        iocs = self._with_concurency(file_data, ioc_types=input_type)
-        #return res
-        #iocs = find_iocs(input_string, included_ioc_types=ioc_types)
-        newarray = []
-        for key, value in iocs.items():
-            if input_type != "all":
-                if key not in input_type:
-                    continue
-
-            if len(value) > 0:
-                for item in value:
-                    # If in here: attack techniques. Shouldn't be 3 levels so no
-                    # recursion necessary
-                    if isinstance(value, dict):
-                        for subkey, subvalue in value.items():
-                            if len(subvalue) > 0:
-                                for subitem in subvalue:
-                                    data = {
-                                        "data": subitem,
-                                        "data_type": "%s_%s" % (key[:-1], subkey),
-                                    }
-                                    if data not in newarray:
-                                        newarray.append(data)
-                    else:
-                        data = {"data": item, "data_type": key[:-1]}
-                        if data not in newarray:
-                            newarray.append(data)
-
-        # Reformatting IP
-        for item in newarray:
-            if "ip" in item["data_type"]:
-                item["data_type"] = "ip"
-                try:
-                    item["is_private_ip"] = ipaddress.ip_address(item["data"]).is_private
-                except:
-                    self.logger.info("Error parsing %s" % item["data"])
-
-        try:
-            newarray = json.dumps(newarray)
-        except json.decoder.JSONDecodeError as e:
-            return "Failed to parse IOC's: %s" % e
-
-        return newarray
 
     def parse_ioc(self, input_string, input_type="all"):
-        #if len(input_string) > 2500000 and (input_type == "" or input_type == "all"):
-        #    return {
-        #        "success": False,
-        #        "reason": "Data too large (%d). Please reduce it below 2.5 Megabytes to use this action or specify the input type" % len(input_string)
-        #    }
+        ioc_types = ["domains", "urls", "email_addresses", "ipv4s", "ipv4_cidrs", "md5s", "sha256s", "sha1s", "cves"]
 
-        # https://github.com/fhightower/ioc-finder/blob/6ff92a73a60e9233bf09b530ccafae4b4415b08a/ioc_finder/ioc_finder.py#L433
-        ioc_types = ["domains", "urls", "email_addresses", "ipv6s", "ipv4s", "ipv4_cidrs", "md5s", "sha256s", "sha1s", "cves"]
-        input_string = str(input_string)
-        if input_type == "":
+        # Remember overriding ioc types we care about
+        if input_type == "" or input_type == "all":
             input_type = "all"
         else:
             input_type = input_type.split(",")
@@ -2659,6 +2501,159 @@ class Tools(AppBase):
             return "Failed to parse IOC's: %s" % e
 
         return newarray
+    
+
+    def split_text(self, text):
+        # Split text into chunks of 10kb. Add each 10k to array
+        # In case e.g. 1.2.3.4 lands exactly on 20k boundary, it may be useful to overlap here.
+        # (just shitty code to reduce chance of issues) while still going fast
+        arr_one = []
+        max_len = 5000 
+        current_string = ""
+        overlaps = 100 
+
+        for i in range(0, len(text)):
+            current_string += text[i]
+            if len(current_string) > max_len:
+                # Appending just in case even with overlaps
+                if len(text) > i+overlaps:
+                    current_string += text[i+1:i+overlaps]
+                else:
+                    current_string += text[i+1:]
+
+                arr_one.append(current_string)
+                current_string = ""
+
+        if len(current_string) > 0:
+            arr_one.append(current_string)
+
+        return arr_one 
+
+    def _format_result(self, result):
+        final_result = {}
+        
+        for res in result:
+            for key,val in res.items():
+                if key in final_result:
+                    if isinstance(val, list) and len(val) > 0:
+                        for i in val:
+                            final_result[key].append(i)
+                    elif isinstance(val, dict):
+                        #print(key,":::",val)
+                        if key in final_result:
+                            if isinstance(val, dict):
+                                for k,v in val.items():
+                                    #print("k:",k,"v:",v)
+                                    val[k].append(v)
+                        #print(val)
+                    #final_result[key].append([i for i in val if len(val) > 0])
+                else:
+                    final_result[key] = val
+
+        return final_result
+
+    # See function for how it works~: parse_ioc_new(..)
+    def _with_concurency(self, array_of_strings, ioc_types):
+        results = []
+        #start = time.perf_counter()
+
+        # Workers dont matter..?
+        # What can we use instead? 
+        print("Strings:", len(array_of_strings))
+
+        workers = 4
+        with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+            # Submit the find_iocs function for each string in the array
+            futures = [executor.submit(
+                find_iocs, 
+                text=string, 
+                included_ioc_types=ioc_types,
+            ) for string in array_of_strings]
+
+            # Wait for all tasks to complete
+            concurrent.futures.wait(futures)
+
+            # Retrieve the results if needed
+            results = [future.result() for future in futures]
+        
+        #print("Total time taken:", time.perf_counter()-start)
+        return self._format_result(results)
+
+    # FIXME: Make this good and actually faster than normal
+    # For now: Concurrency doesn't make it faster due to GIL in python.
+    # May need to offload this to an executable or something 
+    def parse_ioc_new(self, input_string, input_type="all"):
+        if input_type == "":
+            input_type = "all"
+
+        ioc_types = ["domains", "urls", "email_addresses", "ipv4s", "ipv4_cidrs", "md5s", "sha256s", "sha1s", "cves"]
+
+        if input_type == "" or input_type == "all":
+            ioc_types = ioc_types
+        else:
+            input_type = input_type.split(",")
+            for item in input_type:
+                item = item.strip()
+
+            ioc_types = input_type
+
+        input_string = str(input_string)
+
+        if len(input_string) > 10000:
+            iocs = self._with_concurency(self.split_text(input_string), ioc_types=ioc_types)
+        else:
+            iocs = find_iocs(input_string, included_ioc_types=ioc_types)
+
+        newarray = []
+        for key, value in iocs.items():
+            if input_type != "all":
+                if key not in input_type:
+                    continue
+    
+            if len(value) == 0:
+                continue
+
+            for item in value:
+                # If in here: attack techniques. Shouldn't be 3 levels so no
+                # recursion necessary
+                if isinstance(value, dict):
+                    for subkey, subvalue in value.items():
+                        if len(subvalue) == 0:
+                            continue
+
+                        for subitem in subvalue:
+                            data = {
+                                "data": subitem,
+                                "data_type": "%s_%s" % (key[:-1], subkey),
+                            }
+
+                            if data not in newarray:
+                                newarray.append(data)
+                else:
+                    data = {"data": item, "data_type": key[:-1]}
+                    if data not in newarray:
+                        newarray.append(data)
+
+        # Reformatting IP
+        i = -1
+        for item in newarray:
+            i += 1
+            if "ip" not in item["data_type"]:
+                continue
+
+            newarray[i]["data_type"] = "ip"
+            try:
+                newarray[i]["is_private_ip"] = ipaddress.ip_address(item["data"]).is_private
+            except Exception as e:
+                print("Error parsing %s: %s" % (item["data"], e))
+
+        try:
+            newarray = json.dumps(newarray)
+        except json.decoder.JSONDecodeError as e:
+            return "Failed to parse IOC's: %s" % e
+
+        return newarray
+    
 
 if __name__ == "__main__":
     Tools.run()
