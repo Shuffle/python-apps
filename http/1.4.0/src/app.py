2ebdf2cbb1853eed04ee38207624ba5dab5c7c9c
Frikky <frikky@shuffler.io>
2025-03-15 14:51:07 +0100

    Fixed some run issues with http 1.3.0

--- http/1.4.0/src/app.py
+++ http/1.4.0/src/app.py
@@ -8,10 +8,10 @@ import asyncio
 import requests
 import subprocess
 
-from walkoff_app_sdk.app_base import AppBase
+from shuffle_sdk import AppBase
 
 class HTTP(AppBase):
-    __version__ = "1.3.0"
+    __version__ = "1.4.0"
     app_name = "http"  
 
     def __init__(self, redis, logger, console_logger=None):
@@ -439,10 +439,7 @@ class HTTP(AppBase):
 
 # Run the actual thing after we've checked params
 def run(request):
-    print("Starting cloud!")
     action = request.get_json() 
-    print(action)
-    print(type(action))
     authorization_key = action.get("authorization")
     current_execution_id = action.get("execution_id")
 	
