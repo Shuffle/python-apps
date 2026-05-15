bf386af15d91cfa07b53af2a6a028c23a3e88e51
SecEnthusiast76 <secenthusiast76@no-reply.org>
2026-05-15 11:33:42 +0200

    Fixing an issue in the postgress app where Shuffle would not be able to serialize results to JSON as it would see RealDictObject instead. Adding App Authentication support to postgress app.

--- postgress/1.1.0/src/app.py
+++ postgress/1.1.0/src/app.py
@@ -1,18 +1,18 @@
-import psycopg2
+from psycopg2 import connect, ProgrammingError
 from psycopg2.extras import RealDictCursor
-#from walkoff_app_sdk.app_base import AppBase
 from shuffle_sdk import AppBase
 
 
 class PostgreSQL(AppBase):
-    __version__ = "1.0.0"
+    __version__ = "1.1.0"
     app_name = "PostgreSQL"
 
     def __init__(self, redis, logger, console_logger=None):
         super().__init__(redis, logger, console_logger)
 
-    def connect(self, host, port, dbname, user, password):
-        conn = psycopg2.connect(
+    @staticmethod
+    def connect(host, port, dbname, user, password):
+        conn = connect(
             host=host,
             port=port,
             dbname=dbname,
@@ -24,12 +24,13 @@ class PostgreSQL(AppBase):
 
     def run_query(self, host, port, dbname, user, password, query):
         with self.connect(host, port, dbname, user, password) as conn:
-            with conn.cursor() as cur:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
                 cur.execute(query)
                 try:
-                    return {"result": cur.fetchall()}
-                except psycopg2.ProgrammingError:
+                    return {"result": [dict(row) for row in cur.fetchall() if row]}
+                except ProgrammingError:
                     return {"message": "Query executed successfully, no data returned."}
+
 
 if __name__ == "__main__":
     PostgreSQL.run()
